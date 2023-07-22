580e6e232867f78c6fadc8adac336e85be0627eb
AhmedFergany1 <mohamedmido012002897@gmail.com>
2023-07-22 18:02:38 +0300

    first push

---
+++ get_familiar_with_data/ExploreDataSet.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+
+df = pd.read_csv('/home/mustafa7egazi/PycharmProjects/pythonProject/dataset.csv')
+print(df.head())
+
+# print(df.isnull().sum())
+# print(len(df['label'].unique()))
+# print(df['label'].value_counts())
+# print(df['label'].describe())
